1477e0ccc8e13f44f112bea7e5d7b1b7e2f8a68a
nkondratyk <nkondratyk@playtika.com>
2021-10-20 16:28:31 +0300

    Initial release

--- docker_backup_windows.ps1
+++ docker_backup_windows.ps1
@@ -1,12 +1,11 @@
-$current_date = Get-Date -Format "MM.dd.yyyy.HH.mm"
-
 # Clear previous images and volumes
-rm $PSScriptRoot/backup/volumes/*
-rm $PSScriptRoot/backup/images/*
+rm -rf $pwd/backup/volumes/*
+rm -rf $pwd/backup/images/*
+rm -rf $pwd/archives/*
 
 # Save Docker Volumes
-docker run --rm --volumes-from mariadb -v $PSScriptRoot/backup/volumes:/backup ubuntu tar Pcvf /backup/mariadb_data.tar /bitnami/mariadb
-docker run --rm --volumes-from elasticsearch -v $PSScriptRoot/backup/volumes:/backup ubuntu tar Pcvf /backup/elasticsearch_data.tar /usr/share/elasticsearch/data
+docker run --rm --volumes-from mariadb -v $pwd/backup/volumes:/backup ubuntu tar Pcvf /backup/mariadb_data.tar /bitnami/mariadb
+docker run --rm --volumes-from elasticsearch -v $pwd/backup/volumes:/backup ubuntu tar Pcvf /backup/elasticsearch_data.tar /usr/share/elasticsearch/data
 
 # Save changes in Images
 docker commit elasticsearch db_monitoring_stack_elasticsearch:latest
@@ -17,14 +16,13 @@ docker commit mariadb mariadb:latest
 docker commit apm-server apm-server:latest
 
 # Download Images
-docker save db_monitoring_stack_elasticsearch:latest > $PSScriptRoot/backup/images/db_monitoring_stack_elasticsearch.zip
-docker save db_monitoring_stack_grafana:latest > $PSScriptRoot/backup/images/db_monitoring_stack_grafana.zip
-docker save db_monitoring_stack_logstash:latest > $PSScriptRoot/backup/images/db_monitoring_stack_logstash.zip
-docker save db_monitoring_stack_kibana:latest > $PSScriptRoot/backup/images/db_monitoring_stack_kibana.zip
-docker save mariadb:latest > $PSScriptRoot/backup/images/mariadb.zip
-docker save apm-server:latest > $PSScriptRoot/backup/images/apm-server.zip
-docker save ubuntu:latest > $PSScriptRoot/backup/images/ubuntu.zip
+docker save db_monitoring_stack_elasticsearch:latest > $pwd/backup/images/db_monitoring_stack_elasticsearch.tar
+docker save db_monitoring_stack_grafana:latest > $pwd/backup/images/db_monitoring_stack_grafana.tar
+docker save db_monitoring_stack_logstash:latest > $pwd/backup/images/db_monitoring_stack_logstash.tar
+docker save db_monitoring_stack_kibana:latest > $pwd/backup/images/db_monitoring_stack_kibana.tar
+docker save mariadb:latest > $pwd/backup/images/mariadb.tar
+docker save apm-server:latest > $pwd/backup/images/apm-server.tar
+docker save ubuntu:latest > $pwd/backup/images/ubuntu.tar
 
 # Create single archive
-Compress-Archive -Path $PSScriptRoot -DestinationPath $PSScriptRoot/archives/db_monitoring_stack_$current_date.zip -Exclude $PSScriptRoot/archives
-
+Compress-Archive -Path $pwd -DestinationPath $pwd/archives/db_monitoring_stack_full.zip
